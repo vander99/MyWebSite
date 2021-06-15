ace404d6937014a30220cde67cc07f22bb1f5a04
Ismail Jemmoudi <ismail.jemmoudi@gmail.com>
2021-06-15 14:00:10 +0200

    update personnal informations

--- main/js/main.js
+++ main/js/main.js
@@ -305,6 +305,23 @@ function bodyScrollingToggle(){
     })
 })();
 
+window.addEventListener("load",()=>{
+    // preloader
+    document.querySelector(".preloader").classList.add("fade-out");
+    setTimeout(()=>{
+        document.querySelector(".preloader").style.display="none";
+    },600)
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
